3a1fb1600e9f42bff0334189e246d64817c3c720
EnricoRomanoPH <enrico.romano@libero.it>
2025-09-07 09:39:12 +0200

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -1,44 +1,26 @@
-let currentIndex = 0;
-let imagesList = [];
-
 function loadGallery(jsonFile, containerId) {
   fetch(jsonFile)
     .then(response => response.json())
     .then(images => {
-      imagesList = images; // salva la lista globale
       const container = document.getElementById(containerId);
-      images.forEach((imgPath, index) => {
+      images.forEach(imgPath => {
         const img = document.createElement("img");
         img.src = imgPath;
         img.alt = "foto";
-        img.addEventListener("click", () => openLightbox(index));
+        img.addEventListener("click", () => openLightbox(imgPath));
         container.appendChild(img);
       });
     })
     .catch(err => console.error("Errore nel caricamento:", err));
 }
 
-function openLightbox(index) {
-  currentIndex = index;
+function openLightbox(src) {
   const lightbox = document.getElementById("lightbox");
   const lightboxImg = document.getElementById("lightbox-img");
   lightbox.style.display = "flex";
-  lightboxImg.src = imagesList[currentIndex];
+  lightboxImg.src = src;
 }
 
-// Chiudi lightbox
 document.getElementById("close")?.addEventListener("click", () => {
   document.getElementById("lightbox").style.display = "none";
-});
-
-// Freccia indietro
-document.getElementById("prev")?.addEventListener("click", () => {
-  currentIndex = (currentIndex - 1 + imagesList.length) % imagesList.length;
-  document.getElementById("lightbox-img").src = imagesList[currentIndex];
-});
-
-// Freccia avanti
-document.getElementById("next")?.addEventListener("click", () => {
-  currentIndex = (currentIndex + 1) % imagesList.length;
-  document.getElementById("lightbox-img").src = imagesList[currentIndex];
 });
